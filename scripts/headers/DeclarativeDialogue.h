6723c76bc97c6c8aaf03cee69017081fa4163468
Mrowr Purr <mrowr.purr@gmail.com>
2022-07-10 12:14:07 -0700

    Remove test files

--- scripts/headers/DeclarativeDialogue.h
+++ scripts/headers/DeclarativeDialogue.h
@@ -33,3 +33,24 @@ variable __dialogue_procedure_invocation_result__;
     __dialogue_procedure_name_to_invoke__ = proc_name_as_string; \
     __dialogue_procedure_invocation_result__ = __dialogue_procedure_name_to_invoke__(); \
     debug_msg("dialogue proc " + __dialogue_procedure_name_to_invoke__ + "() result: " + __dialogue_procedure_invocation_result__) // You *must* use the result or the optimizer won't invoke the procedure at all!
+
+procedure __dialogue__empty_proc_for_end__ begin
+end
+
+procedure __dialogue_begin__ begin
+
+end
+
+    // start_gdialog(NAME,self_obj,4,-1,-1);
+    //      gSay_Start;
+    //         call Node014;
+    //      gSay_End;
+    //      end_dialogue;
+
+//       if( ( dude_iq == 2 ) or ( dude_iq == 3 ) ) then
+//       NOption(112,Node006,002);
+//    if (dude_strength < 8) then
+//       GOption(113,Node007,004);
+//    else
+//       NOption(114,Node006,004);
+//    GOption(115,Node007,004);
